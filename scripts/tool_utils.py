247c1dc56a99c7a4b6a53671dbda300f2b12aa65
lkoolik <94492257+lkoolik@users.noreply.github.com>
2026-04-14 11:37:00 -0700

    Update tool_utils.py

--- scripts/tool_utils.py
+++ scripts/tool_utils.py
@@ -205,7 +205,7 @@ def report_version():
 
     logging.info('╔════════════════════════════════╗')
     logging.info('║ ECHO-AIR Model                 ║')
-    logging.info('║ Version 1.0.5                  ║')
+    logging.info('║ Version 1.1.0                  ║')
     logging.info('╚════════════════════════════════╝')
     logging.info('\n')
     return
